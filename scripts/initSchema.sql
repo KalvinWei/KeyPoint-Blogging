9047287a384d15681cbdc083fe8d8d7019e5bd2b
CoderYihaoWang <yesiamyihao@gmail.com>
2020-06-09 20:57:23 +1200

    delete to do

--- scripts/initSchema.sql
+++ scripts/initSchema.sql
@@ -1,4 +1,3 @@
--- TODO: Add your database init script here. This should initialize all your tables, and add any initial data required.
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS article;
 DROP TABLE IF EXISTS comment;
