a774810959010b2f0f83b6773d264fa6bb2f2d57
CoderYihaoWang <yesiamyihao@gmail.com>
2020-06-10 15:24:13 +1200

    add url patterns to action servlets

--- src/ictgradschool/project/servlet/action/SignUp.java
+++ src/ictgradschool/project/servlet/action/SignUp.java
@@ -1,11 +1,13 @@
 package ictgradschool.project.servlet.action;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "SignUp", urlPatterns = {"/signUp"})
 public class SignUp extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
