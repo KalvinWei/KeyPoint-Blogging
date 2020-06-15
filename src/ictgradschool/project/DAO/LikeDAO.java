60b1569ecebd97070b8f32115b05573fa498dd12
CoderYihaoWang <yesiamyihao@gmail.com>
2020-06-15 14:01:31 +1200

    finish likeDAO

--- src/ictgradschool/project/DAO/LikeDAO.java
+++ src/ictgradschool/project/DAO/LikeDAO.java
@@ -1,31 +1,98 @@
 package ictgradschool.project.DAO;
 
 import ictgradschool.project.model.User;
+import ictgradschool.project.util.DBConnectionUtils;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class LikeDAO {
-    public static boolean checkLikeArticleStatus(int userId, int articleId) {
+    public static boolean checkLikeArticleStatus(int userId, int articleId) throws IOException, SQLException {
+        try (Connection conn = DBConnectionUtils.getConnectionFromClasspath("connection.properties")) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) AS count FROM likeArticle WHERE user = ? AND article = ?")) {
+                ps.setInt(1, userId);
+                ps.setInt(2, articleId);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        int count = rs.getInt(1);
+                        return count != 0;
+                    }
+                }
+            }
+        }
         return false;
     }
 
-    public static boolean checkLikeCommentStatus(int userId, int commentId) {
+    public static boolean checkLikeCommentStatus(int userId, int commentId) throws IOException, SQLException {
+        try (Connection conn = DBConnectionUtils.getConnectionFromClasspath("connection.properties")) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) AS count FROM likeComment WHERE user = ? AND comment = ?")) {
+                ps.setInt(1, userId);
+                ps.setInt(2, commentId);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        int count = rs.getInt(1);
+                        return count != 0;
+                    }
+                }
+            }
+        }
         return false;
     }
 
-    public static boolean likeArticle(int userId, int articleId) {
-        return false;
+    public static boolean likeArticle(int userId, int articleId) throws IOException, SQLException {
+        try (Connection conn = DBConnectionUtils.getConnectionFromClasspath("connection.properties")) {
+            try (PreparedStatement ps = conn.prepareStatement(
+                    "INSERT INTO likeArticle\n" +
+                            "SELECT ?, ?\n" +
+                            "WHERE NOT EXISTS(SELECT * FROM likeArticle WHERE user = ? AND article = ?)"
+            )) {
+                ps.setInt(1, userId);
+                ps.setInt(2, articleId);
+                ps.setInt(3, userId);
+                ps.setInt(4, articleId);
+                return ps.executeUpdate() != 0;
+            }
+        }
     }
 
-    public static boolean likeCommentId(int userId, int commentId) {
-        return false;
+    public static boolean likeComment(int userId, int commentId) throws IOException, SQLException {
+        try (Connection conn = DBConnectionUtils.getConnectionFromClasspath("connection.properties")) {
+            try (PreparedStatement ps = conn.prepareStatement(
+                    "INSERT INTO likeComment\n" +
+                            "SELECT ?, ?\n" +
+                            "WHERE NOT EXISTS(SELECT * FROM likeComment WHERE user = ? AND comment = ?)"
+            )) {
+                ps.setInt(1, userId);
+                ps.setInt(2, commentId);
+                ps.setInt(3, userId);
+                ps.setInt(4, commentId);
+                return ps.executeUpdate() != 0;
+            }
+        }
     }
 
-    public static boolean unlikeArticle(int userId, int articleId) {
-        return false;
+    public static boolean unlikeArticle(int userId, int articleId) throws IOException, SQLException {
+        try (Connection conn = DBConnectionUtils.getConnectionFromClasspath("connection.properties")) {
+            try (PreparedStatement ps = conn.prepareStatement("DELETE FROM likeArticle WHERE user = ? AND article = ?")) {
+                ps.setInt(1, userId);
+                ps.setInt(2, articleId);
+
+                return ps.executeUpdate() != 0;
+            }
+        }
     }
 
-    public static boolean unlikeComment(int userId, int commentId) {
-        return false;
+    public static boolean unlikeComment(int userId, int commentId) throws IOException, SQLException {
+        try (Connection conn = DBConnectionUtils.getConnectionFromClasspath("connection.properties")) {
+            try (PreparedStatement ps = conn.prepareStatement("DELETE FROM likeComment WHERE user = ? AND comment = ?")) {
+                ps.setInt(1, userId);
+                ps.setInt(2, commentId);
+                return ps.executeUpdate() != 0;
+            }
+        }
     }
 }
