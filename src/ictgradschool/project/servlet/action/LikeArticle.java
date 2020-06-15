9915ad1efb18c450dfbc205e2d93a4c096f89aa3
CoderYihaoWang <yesiamyihao@gmail.com>
2020-06-15 14:53:18 +1200

    done follow/unfollow, like/unlike back end

--- src/ictgradschool/project/servlet/action/LikeArticle.java
+++ src/ictgradschool/project/servlet/action/LikeArticle.java
@@ -1,4 +1,31 @@
 package ictgradschool.project.servlet.action;
 
-public class LikeArticle {
+import ictgradschool.project.DAO.FollowDAO;
+import ictgradschool.project.DAO.LikeDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "LikeArticle", urlPatterns = {"/likeArticle"})
+public class LikeArticle extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp); // do not support
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int user = Integer.parseInt(req.getParameter("user"));
+        int article = Integer.parseInt(req.getParameter("article"));
+        try {
+            LikeDAO.likeArticle(user, article);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
