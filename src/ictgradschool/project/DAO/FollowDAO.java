9fc857946d842b6d602c87019b62c10934a9af48
CoderYihaoWang <yesiamyihao@gmail.com>
2020-06-15 12:28:39 +1200

    create stub methods in followDAO and likeDAO

--- src/ictgradschool/project/DAO/FollowDAO.java
+++ src/ictgradschool/project/DAO/FollowDAO.java
@@ -1,4 +1,11 @@
 package ictgradschool.project.DAO;
 
 public class FollowDAO {
+    public static boolean checkFollowStatus(int followeeId, int followerId) {
+        return false;
+    }
+
+    public static boolean follow(int followeeId, int followerId) {
+        return false;
+    }
 }
--- src/ictgradschool/project/DAO/LikeDAO.java
+++ src/ictgradschool/project/DAO/LikeDAO.java
@@ -1,4 +1,19 @@
 package ictgradschool.project.DAO;
 
 public class LikeDAO {
+    public static boolean checkLikeArticleStatus(int userId, int articleId) {
+        return false;
+    }
+
+    public static boolean checkLikeCommentStatus(int userId, int commentId) {
+        return false;
+    }
+
+    public static boolean likeArticle(int userId, int articleId) {
+        return false;
+    }
+
+    public static boolean likeCommentId(int userId, int commentId) {
+        return false;
+    }
 }
